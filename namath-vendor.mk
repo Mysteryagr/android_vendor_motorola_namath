15cf61fb26a60752f4555971d9885e1a0829f4d4
Mysteryagr <ahmedrahmy842@gmail.com>
2017-12-03 23:32:34 +0000

    First commit

---
+++ namath-vendor.mk
@@ -0,0 +1,19 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/lib,system/lib)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/priv-app,system/priv-app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/namath/vendor/thh,system/vendor/thh)
